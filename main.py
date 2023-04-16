924a1a1a151646c0e86bbd01c8c4a67a520323ea
Geo-Schon <dr-gudkov@mail.ru>
2023-04-16 12:27:22 +0300

    Версия 2.0

--- main.py
+++ main.py
@@ -1,42 +1,64 @@
 import requests
-import csv
-from bs4 import BeautifulSoup
+import os
+from bs4 import BeautifulSoup as bs
 from fake_headers import Headers
+from tqdm import tqdm
+import re
+import json
 
-header = Headers(browser='firefox', os='win')
-base_url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area'
-request = requests.get(f'{base_url}', headers=header.generate()).text
-soup = BeautifulSoup(request, 'lxml')
 
-urls = []
-pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
-count_pages = int(pagination[-1].text)
-for i in range(count_pages):
-    url = f'https://spb.hh.ru/search/vacancy?text=python&area=1&area={i}'
-    if url not in urls:
-        urls.append(url)
-        print(url)
+def get_headers():
+    return Headers(browser="chrome", os="win").generate()
 
-parsed_data = []
-divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
-for div in divs:
-    vac = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})
-    title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-    href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
-    company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-    description1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
-    description2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
-    description = description1 + description2
-    parsed_data.append({
-        'title': title,
-        'href': href,
-        'company': company,
-        'description': description})
 
-with open('jobs.csv', 'w', newline='') as file:
-    a_pen = csv.writer(file, delimiter=',')
-    a_pen.writerow(('Название вакансии', 'URL', 'Название комании', 'Описание'))
-    for job in parsed_data:
-            a_pen.writerow((job['title'], job['href'], job['company'], job['description']))
+def main_page_search(url):
+    hh_main_html = requests.get(url=url, headers=get_headers()).text
+    hh_main_soup = bs(hh_main_html, "lxml")
+    tag_all_vacancy = hh_main_soup.find_all("div", class_="serp-item")
+    return tag_all_vacancy
 
-print(parsed_data)
+
+def job_search(tag_vacancy):
+    link_vacancy = tag_vacancy.find("a", class_="serp-item__title")["href"]
+    if tag_vacancy.find("span", class_="bloko-header-section-3") is None:
+        salary_vacancy = f"Зарлата не указана"
+    else:
+        salary_vacancy = tag_vacancy.find('span', class_='bloko-header-section-3').text
+    name_company_vacancy = tag_vacancy.find("a", "bloko-link bloko-link_kind-tertiary").text
+    title = tag_vacancy.find('a', class_='serp-item__title').text
+    tag_description = requests.get(url=link_vacancy, headers=get_headers()).text
+    tag_description_soup = bs(tag_description, "lxml")
+    description_vacancy = tag_description_soup.find("div", class_="g-user-content").text
+    return {
+        "name": title,
+        "link": link_vacancy,
+        "salary": salary_vacancy,
+        "name_company": name_company_vacancy,
+        "description": description_vacancy
+    }
+
+
+def query_search(main_page):
+    parsed_data = []
+    for tag_vacancy in tqdm(main_page):
+        jobs_found = job_search(tag_vacancy)
+        jobs_desc = jobs_found["description"]
+        pattern = re.findall(
+            r"([Dd]jango.*[Ff]lask)|([Ff]lask.*[Dd]jango)|([Ff]lask.*\s*.*[Dd]jango)", jobs_desc,)
+        if pattern != []:
+            _ = jobs_found.pop("description", None)
+            parsed_data.append(jobs_found)
+    return parsed_data
+
+
+def write_file(file_name, total_list):
+    current = os.getcwd()
+    full_path = os.path.join(current, file_name)
+    with open(full_path, "w", encoding="utf=8") as w:
+        json.dump(total_list, w, ensure_ascii=False, indent=2)
+
+
+if __name__ == "__main__":
+    main_page = main_page_search("https://spb.hh.ru/search/vacancy?text=python&area=1&area=2")
+    result = query_search(main_page)
+    write_file("jobs.json", result)
